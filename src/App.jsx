afde14feee0d56d4ccff2cf7a5a7e1f5d0903ed2
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-10-26 15:57:48 +0900

    updata var.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  const [incompleteTodos, setIncompleteTodos] = useState(["ポテト", "ペン"]);
+  const [completeTodos, setCompleteTodos] = useState(["ハンク"]);
+
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              //keyを忘れないように!!
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>さしすせポテト</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
